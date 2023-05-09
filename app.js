047bb122d567cfa68f750cfebb9203562c491208
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-05-09 10:28:42 +0000

    Fix: tested all the routes and ready for deployment

--- app.js
+++ app.js
@@ -40,7 +40,10 @@ app.use(limiter);
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://myAtlasDBUser:EVie1234@myatlasclusteredu.phleaay.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://myAtlasDBUser:EVie1234@myatlasclusteredu.phleaay.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 
 main().catch((err) => console.log(err));
 async function main() {
